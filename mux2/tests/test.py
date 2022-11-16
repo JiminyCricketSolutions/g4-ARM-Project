56e238a9c3cda2e004dd96d992814168a5e9187a
riolan <riolaney@att.net>
2022-11-16 04:26:52 +0000

    Updating mux2 model and test .py

--- mux2/tests/test.py
+++ mux2/tests/test.py
@@ -2,17 +2,21 @@ from model import model
 
 import cocotb
 from cocotb.triggers import Timer
-
+from cocotb.types import LogicArray, Logic, Range
 @cocotb.test()
 async def test4_33(dut):
-
-    for aVal in range(2**4):
-        for bVal in range(2**4):
-            dut.a.value = aVal
-            dut.b.value = bVal
-            await Timer(1, units="ns")
-            hdlResult = int(dut.y.value)
-            modelResult = model(a=dut.a.value, b=dut.b.value).integer
-            assert hdlResult == modelResult, \
+    aVals = [LogicArray(x, Range(8-1, 'downto', 0)) for x in range(0, 2**8)]
+    bVals = [LogicArray(x, Range(8-1, 'downto', 0)) for x in range(0, 2**8)]
+    sel =  [0, 1]
+    for aVal in aVals:
+        for bVal in bVals:
+            for s in sel:
+                dut.d0.value = aVal
+                dut.d1.value = bVal
+                dut.s.value = s
+                await Timer(1, units="ns")
+                hdlResult = int(dut.y.value)
+                modelResult = model(d0=dut.d0.value, d1=dut.d1.value, s=dut.s.value).integer
+                assert hdlResult == modelResult, \
                 f"HDL and model disagree: {hdlResult} vs {modelResult}"
 
