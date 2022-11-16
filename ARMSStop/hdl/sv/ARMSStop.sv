651b1059ed4d77a0fa4b1153d27bf0753c3222e8
providence <jiminycricketsolutions@gmail.com>
2022-11-15 20:04:27 -0600

    Fulfilled top module as ARMSStop from the book

---
+++ ARMSStop/hdl/sv/ARMSStop.sv
@@ -0,0 +1,30 @@
+module ARMSStop (
+  input   logic       clk, reset,
+  output  logic [31:0] WriteData, DataAdr,
+  output  logic       MemWrite
+);
+
+  logic [31:0] PC, Instr, ReadData;
+
+  // instantiate processor and memories
+  ARMSS arm(clk, reset, PC, Instr, MemWrite, DataAdr, WriteData, ReadData);
+  imem  imem(PC, Instr);
+  dmem  dmem(clk, MemWrite, DataAdr, WriteData, ReadData);
+
+  // so cocotb will output the wave file in the tests directory
+  `ifdef  COCOTB_SIM 
+  initial
+   begin
+      $dumpfile("wave_sv.vcd");
+      $dumpvars(
+        clk,
+        reset,
+        WriteData,
+        DataAdr,
+        MemWrite
+      );
+      #5;
+   end
+  `endif
+
+endmodule
