10104df8909f62de2f2162e94c0f5d2d9d2a675f
providence <jiminycricketsolutions@gmail.com>
2022-11-16 08:39:25 -0600

    Fulfilled imem module from book

---
+++ imem/hdl/sv/imem.sv
@@ -0,0 +1,26 @@
+module imem (
+  input   logic [31:0] a,
+  output  logic [31:0] rd
+);
+
+  logic [31:0] RAM[63:0];
+
+  initial
+    $readmemh("memfile.dat", RAM);
+    assign rd = RAM[a[31:2]]; // word alligned
+
+  // so cocotb will output the wave file in the tests directory
+  `ifdef  COCOTB_SIM 
+  initial
+   begin
+      $dumpfile("wave_sv.vcd");
+      $dumpvars(
+        0,
+        a,
+        rd
+      );
+      #5;
+   end
+  `endif
+
+endmodule
