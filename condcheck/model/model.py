1bf70ff6261994ff0196548dda26038301529620
riolan <riolaney@att.net>
2022-11-18 01:08:56 +0000

    Updated condlogic model and tests, had some logicarray weirdness refer back if necessary.

--- condcheck/model/model.py
+++ condcheck/model/model.py
@@ -1,5 +1,55 @@
 from cocotb.types import Logic, LogicArray, Range
 
-def model(a: LogicArray, b: LogicArray) -> LogicArray:
-    """ model for 4_33 unsigned multiplier """
-    return LogicArray(a.integer * b.integer)
+def model(Cond: LogicArray, Flags: LogicArray) -> LogicArray:
+    """ model for condcheck """
+    neg = Flags[0]
+    zero = Flags[1]
+    carry = Flags[2]
+    overflow = Flags[3]
+
+    # weird workaround of bin.BinaryValue or whatever, covert the logic
+    # from bools to logicarray and decode to int
+    ge = LogicArray(neg == overflow)
+    Cond = Cond.integer
+
+
+    if (Cond == LogicArray('0000').integer):
+        return zero
+    elif (Cond == LogicArray('0001').integer):
+        return LogicArray(not(zero.integer))
+    elif (Cond == LogicArray('0010').integer):
+        return carry
+    elif (Cond == LogicArray('0011').integer):
+        return LogicArray(not(carry.integer))
+#        return ~carry
+    elif (Cond == LogicArray('0100').integer):
+        return neg
+    elif (Cond == LogicArray('0101').integer):
+        return LogicArray(not(neg.integer))
+#        return ~neg
+    elif (Cond == LogicArray('0110').integer):
+        return overflow
+    elif (Cond == LogicArray('0111').integer):
+        return LogicArray(not(overflow.integer))
+#        return ~overflow
+    elif (Cond == LogicArray('1000').integer):
+        return LogicArray(carry and not(zero.integer))
+#        return carry and ~zero
+    elif (Cond == LogicArray('1001').integer):
+        return LogicArray(not(carry and not(zero.integer)))
+#        return ~(carry and ~zero)
+    elif (Cond == LogicArray('1010').integer):
+        return ge
+    elif (Cond == LogicArray('1011').integer):
+        return LogicArray(not(ge.integer))
+#        return ~ge
+    elif (Cond == LogicArray('1100').integer):
+        return LogicArray(not(zero.integer) and ge.integer)
+#        return ~zero and ge
+    elif (Cond == LogicArray('1101').integer):
+        return LogicArray(not(not(zero.integer) and ge.integer))
+#        return ~(~zero and ge)
+    elif (Cond == LogicArray('1110').integer):
+        return LogicArray('1')
+    else:
+        return LogicArray("X")
--- condcheck/tests/test.py
+++ condcheck/tests/test.py
@@ -2,17 +2,25 @@ from model import model
 
 import cocotb
 from cocotb.triggers import Timer
-
+from cocotb.types import LogicArray, Logic, Range
 @cocotb.test()
 async def testCondcheck(dut):
+    aVals = [LogicArray(x, Range(4-1, 'downto', 0)) for x in range(0, 2**4)]
+    bVals = [LogicArray(x, Range(4-1, 'downto', 0)) for x in range(0, 2**4)]
 
-    for aVal in range(2**4):
-        for bVal in range(2**4):
-            dut.a.value = aVal
-            dut.b.value = bVal
+    for aVal in aVals:
+        for bVal in bVals:
+            dut.Cond.value = aVal
+            dut.Flags.value = bVal
             await Timer(1, units="ns")
-            hdlResult = int(dut.y.value)
-            modelResult = model(a=dut.a.value, b=dut.b.value).integer
+            try:
+                hdlResult = int(dut.CondEx.value)
+            except ValueError:
+                hdlResult = -1 # understood as undefined
+            try:
+                modelResult = model(Cond=dut.Cond.value, Flags=dut.Flags.value).integer
+            except ValueError:
+                modelResult = -1 # understood as: "undefined" 
             assert hdlResult == modelResult, \
                 f"HDL and model disagree: {hdlResult} vs {modelResult}"
 
