b56141430d4a26bbac985c08d81f478654ceb57f
providence <jiminycricketsolutions@gmail.com>
2022-11-28 15:05:10 -0600

    Deprecated regfile module

--- regfile/model/model.py
+++
@@ -1,5 +0,0 @@
-from cocotb.types import Logic, LogicArray, Range
-
-def model(a: LogicArray, b: LogicArray) -> LogicArray:
-    """ model for 4_33 unsigned multiplier """
-    return LogicArray(a.integer * b.integer)
