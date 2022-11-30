7c6b2b214a5f5eb48699bfa087bec3da97d4a35a
riolan <riolaney@att.net>
2022-11-30 03:40:20 +0000

    updated condlogic test

--- condlogic/tests/test.py
+++ condlogic/tests/test.py
@@ -1,18 +1,55 @@
-from model import model
+from model import condlogic as model
 
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, FallingEdge
+from cocotb.types import LogicArray, Logic, Range
+from cocotb.clock import Clock
 
 @cocotb.test()
 async def testCondlogic(dut):
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
 
-    for aVal in range(2**4):
-        for bVal in range(2**4):
-            dut.a.value = aVal
-            dut.b.value = bVal
-            await Timer(1, units="ns")
-            hdlResult = int(dut.y.value)
-            modelResult = model(a=dut.a.value, b=dut.b.value).integer
-            assert hdlResult == modelResult, \
-                f"HDL and model disagree: {hdlResult} vs {modelResult}"
 
+    Cond_ = [LogicArray(x, Range(4-1, 'downto', 0)) for x in range(0, 2**4)]
+    ALUFlags_ = [LogicArray(x, Range(4-1, 'downto', 0)) for x in range(0, 2**4)]
+    FlagW_ = [LogicArray(x, Range(2-1, 'downto', 0)) for x in range(0, 2**2)]
+    PCS_ = [0, 1]
+    RegW_ = [0, 1]
+    MemW_ = [0, 1]
+    reset_ = [0, 1]
+
+    CondlogicModel = model()
+
+    await FallingEdge(dut.clk)
+
+    dut.reset.value = 1
+    n = LogicArray('0000')
+    CondlogicModel.flopenr(1, n, n)
+
+
+    await FallingEdge(dut.clk)
+
+    dut.reset.value = 0
+
+    for Cond in Cond_:
+        for ALUFlags in ALUFlags_:
+            for FlagW in FlagW_:
+                for PCS in PCS_:
+                    for RegW in RegW_:
+                        for MemW in MemW_:
+#                            for reset in reset_:
+#                            dut.reset.value = reset
+                            print('t')
+                            dut.MemW.value = MemW
+                            dut.RegW.value = RegW
+                            dut.PCS.value = PCS
+                            dut.FlagW.value = FlagW
+                            dut.ALUFlags.value = ALUFlags
+                            dut.Cond.value = Cond
+
+                            CondlogicModel.flopenr(0, ALUFlags, FlagW)
+                            modelResult = CondlogicModel.set(Cond, ALUFlags, FlagW, PCS, RegW, MemW)
+
+                            await FallingEdge(dut.clk)
+                            assert dut.PCSrc.value == modelResult["PCSrc"], f"HDL and model result disagree: {dut.PCSrc.value} vs {modelResult['PCSrc']}"
