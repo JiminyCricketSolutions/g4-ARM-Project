9b0193810a88eb81bb7e24640de2c363703600a0
providence <jiminycricketsolutions@gmail.com>
2022-11-30 10:59:08 -0600

    removed floaters, Made test more descriptive

--- condcheck/model/model.py
+++ condcheck/model/model.py
@@ -14,34 +14,49 @@ def model(Cond: LogicArray, Flags: LogicArray):
 
 
     if (Cond == LogicArray('0000').integer):
+        print(f"Cond = {Cond}; Flags: {Flags}; Value returned: zero - {int(zero)}")
         return int(zero)
     elif (Cond == LogicArray('0001').integer):
+        print(f"Cond = {Cond}; Flags: {Flags}; Value returned: ~zero - {int(not(zero))}")
         return int(not(zero))
     elif (Cond == LogicArray('0010').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return carry
     elif (Cond == LogicArray('0011').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return int(not(carry))
     elif (Cond == LogicArray('0100').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return neg
     elif (Cond == LogicArray('0101').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return int(not(neg))
     elif (Cond == LogicArray('0110').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return overflow
     elif (Cond == LogicArray('0111').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return int(not(overflow))
     elif (Cond == LogicArray('1000').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return int(carry and not(zero))
     elif (Cond == LogicArray('1001').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return int(not(carry and not(zero)))
     elif (Cond == LogicArray('1010').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return ge
     elif (Cond == LogicArray('1011').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return int(not(ge))
     elif (Cond == LogicArray('1100').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return int(not(zero) and ge)
     elif (Cond == LogicArray('1101').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return int(not(not(zero) and ge))
     elif (Cond == LogicArray('1110').integer):
+        # print(f"Cond = {Cond}; Value returned: zero - {int(zero)}")
         return 1
     else:
-        return -1
+        return 0
--- condcheck/tests/test.py
+++ condcheck/tests/test.py
@@ -6,7 +6,7 @@ from cocotb.types import LogicArray, Logic, Range
 @cocotb.test()
 async def testCondcheck(dut):
     for aVal in range(0, 2**4-1):
-        for bVal in range(0, 2**4):
+        for bVal in range(0, 2**4-1):
             dut.Cond.value = aVal
             dut.Flags.value = bVal
             fl = LogicArray(bVal, Range(3, "downto", 0))
