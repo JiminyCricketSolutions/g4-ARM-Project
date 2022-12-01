b1b4c9fef08efb6c25a4337d3dbe2241fefeca6a
providence <jiminycricketsolutions@gmail.com>
2022-12-01 08:39:29 -0600

    Made minor change to direction of ARMSS test

--- ARM-single-cycle/tests/test.py
+++ ARM-single-cycle/tests/test.py
@@ -8,8 +8,10 @@ from cocotb.triggers import Timer
 async def testARMSC(dut):
 
 
-    loadedProgram = ARMMEMLOADER.program("ARMSource/ARMAddSubRAW.txt") # I Came in so that 
-    # loadedProgram.getNextInstruction() <- This is a string, pop it in a logicarray
+    loadedProgram = ARMMEMLOADER.program("ARMSource/ARMAddSubRAW.txt") # Bing Bong
+
+    
+    #dut.Instr.value = LogicArray(loadedProgram.getNextInstruction()).Integer # <- This is Sort of the right direction but idk as much about the cocotb types
     # Opening the thing
     # for aVal in range(2**4):
     #     for bVal in range(2**4):
@@ -18,6 +20,6 @@ async def testARMSC(dut):
     #         await Timer(1, units="ns")
     #         hdlResult = int(dut.y.value)
     #         modelResult = model(a=dut.a.value, b=dut.b.value).integer
-            assert False, "Testing Very Incomplete" #\
+    assert False, "Testing Very Incomplete" #\
                 #f"HDL and model disagree: {hdlResult} vs {modelResult}"
 
